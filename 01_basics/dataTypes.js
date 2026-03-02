f87d2b8f1f54bc21694cec686194913cafeb8c7b
divyansh101205 <routraydivyansh@gmail.com>
2026-03-02 08:53:11 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,18 @@
+//alert(3+3)//we arre using nodejs,not browser 
+
+let  name="divyansh";
+let age=18;
+let isLoggedIn=false;
+
+//types of datatypes 
+
+//number
+//string
+//bigint
+//boolean
+//null=>standlone value
+//undefined=>means the value has not been assigned
+//symbol=>unique
+
+
+console.log(typeof null)
---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="routraydivyansh@gmail.com"
+var accountPassword="jaipur"
+accountCity="bbsr"
+
+//accountId=22e22  //not allowed bro
+console.table([accountId,accountEmail,accountPassword,accountCity])
